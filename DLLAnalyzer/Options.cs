6525a1945529d25965881ca686bca40e7dedcdeb
skowront <tomasz.skowron98@gmail.com>
2023-09-18 15:16:21 +0200

    Add project files.

---
+++ DLLAnalyzer/Options.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLLAnalyzer
+{
+    public class Options
+    {
+        public string File { get; set; }
+        public string ClassRegexp { get; set; } = ".*";
+        public string MethodRegexp { get; set; } = ".*";
+        /// <summary>
+        /// c - for classes
+        /// m - for methods 
+        /// f - for full
+        /// </summary>
+        public eDepth Depth {get;set;} = eDepth.Full;
+        
+        public enum eDepth
+        {
+            Full, Class, Method
+        }
+
+        public void Parse(string[] args)
+        {
+            for(int i=0; i < args.Length; i++)
+            {
+                var arg = args[i];
+                if(arg == "-cr")
+                {
+                    this.ClassRegexp = args[i+1];
+                    i++;
+                    continue;
+                }
+                if(arg == "-mr")
+                {
+                    this.MethodRegexp = args[i + 1];
+                    i++;
+                    continue;
+                }
+                if(arg == "-d")
+                {
+                    if (args[i+1] == "method")
+                    {
+                        this.Depth = eDepth.Method;
+                        i++;
+                        continue;
+                    }
+                    if (args[i + 1] == "class")
+                    {
+                        this.Depth = eDepth.Class;
+                        i++;
+                        continue;
+                    }
+                    if (args[i + 1] == "full")
+                    {
+                        this.Depth = eDepth.Full;
+                        i++;
+                        continue;
+                    }
+                }
+                if(i == args.Length-1)
+                {
+                    this.File = args[i];
+                }
+            }
+        }
+    }
+}
---
+++ DLLAnalyzer/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace DLLAnalyzer
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to DLLAnalyzer");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please specify the assembly...");
+                return;
+            }
+            else
+            {
+               Options opts = new Options();
+               opts.Parse(args);
+               Program.Run(opts);
+            }
+        }
+
+        static void Run(Options options)
+        {
+            var assembly = System.Reflection.Assembly.LoadFile(options.File);
+            Type[] types = assembly.GetTypes();
+            foreach (var type in types)
+            {
+                Regex classRegexp = new Regex(options.ClassRegexp);
+                if (!classRegexp.IsMatch(type.FullName??string.Empty))
+                {
+                    continue;
+                }
+                Console.WriteLine("---Type---");
+                Console.WriteLine($"Type name: {type.FullName}");
+                var methods = type.GetMethods();
+                if(options.Depth == Options.eDepth.Method || options.Depth == Options.eDepth.Full)
+                foreach (var method in methods)
+                {
+                    Regex methodRegexp = new Regex(options.MethodRegexp);
+                    if (!methodRegexp.IsMatch(method.Name ?? string.Empty))
+                    {
+                        continue;
+                    }
+                    Console.WriteLine("\t---Method---");
+                    Console.WriteLine($"\tMethod name: {method.Name}");
+                    Console.WriteLine($"\tReturn type: {method.ReturnType}");
+                    Console.WriteLine($"\tReturn parameter: {method.ReturnParameter}");
+
+                    var parameters = method.GetParameters();
+                        
+                    if(options.Depth == Options.eDepth.Full)
+                    for (int i = 0; i < parameters.Length; i++)
+                    {
+                        var parameter = parameters[i];
+                        Console.WriteLine("\t\t---Parameter---");
+                        Console.WriteLine($"\t\tParameter: {i}");
+                        Console.WriteLine($"\t\tParameter name: {parameter.Name}");
+                        Console.WriteLine($"\t\tParameter name: {parameter.ParameterType.FullName}");
+                        Console.WriteLine("\t\t---EndParameter---");
+                        Console.WriteLine(" ");
+                    }
+
+                    Console.WriteLine("\t---EndMethod---");
+                    Console.WriteLine(" ");
+                }
+                Console.WriteLine("---EndType---");
+            }
+        }
+    }
+}
